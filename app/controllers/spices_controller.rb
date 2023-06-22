23f2de74accf6ad1d06fe3908aba015aa84b540c
George Samuel <geosam2708@gmail.com>
2023-06-22 22:28:21 +0300

    added create action

--- app/controllers/spices_controller.rb
+++ app/controllers/spices_controller.rb
@@ -2,4 +2,15 @@ class SpicesController < ApplicationController
     def index
         render json: Spice.all, status: :ok
     end
+
+    def create
+        spice = Spice.create(spice_params)
+        render json: spice, status: :created
+    end
+
+    private
+
+    def spice_params
+    params.permit(:title, :image, :description, :notes, :rating)
+    end
 end
