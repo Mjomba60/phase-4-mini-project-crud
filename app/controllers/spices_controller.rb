ab23b886b2206d9a0357e66c253815efa6268a1f
George Samuel <geosam2708@gmail.com>
2023-06-22 22:38:36 +0300

    Added update action

--- app/controllers/spices_controller.rb
+++ app/controllers/spices_controller.rb
@@ -1,4 +1,5 @@
 class SpicesController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
     def index
         render json: Spice.all, status: :ok
     end
@@ -8,9 +9,23 @@ class SpicesController < ApplicationController
         render json: spice, status: :created
     end
 
+    def update
+        spice = find_spice
+        spice.update(spice_params)
+        render json: spice, status: :ok
+    end
+
     private
 
     def spice_params
     params.permit(:title, :image, :description, :notes, :rating)
+    end
+
+    def render_not_found
+        render json: {error: "Spice not found"}, status: :not_found
+    end
+
+    def find_spice
+        Spice.find_by(id: params[:id])
     end
 end
